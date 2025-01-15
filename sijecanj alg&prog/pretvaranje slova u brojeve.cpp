885b6c5010e84cfe6fec200bb095dbea2a6ff693
Domagoj529 <domagoj.milicevic07@gmail.com>
2025-01-15 11:33:00 +0100

    Add files via upload

---
+++ sijecanj alg&prog/pretvaranje slova u brojeve.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    printf("Unesi slovo: ");
+    cin>>a;
+    cout<<endl;
+    printf("Vrijednost slova je: %i", a);
+return 0;
+}
